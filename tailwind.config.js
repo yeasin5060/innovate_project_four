99b904ce131799d0044d009b9bdecfc0305f40d3
yeasin <yasin9860@gmail.com>
2023-10-05 12:26:30 +0600

    innovate_project_three

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+      fontSize: {
+        heading:"48px",
+        navbar:"18px",
+        pera:"16px"
+      },
+      spacing: {
+        "banner":"1920px",
+        "container":"1410px"
+      }
+    }
+  }
